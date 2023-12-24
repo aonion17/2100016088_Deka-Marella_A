44e1e6a1355abe73855a109df8f12437b66e7700
aonion17 <121542448+aonion17@users.noreply.github.com>
2023-12-24 22:46:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const capitalize = (str) => {
 // @ Try to check and change the filterOddNumber function
 // Ex: given param = [1, 2, 3, 4, 5], then return must [2, 4] not "1,3,5"
 const filterOddNumber = (arr) => {
-  return arr.filter((num) => num % 2 !== 0).toString();
+  return arr.filter((num) => num % 2 === 0);
 };
 /**
- * Write your description here, why the function is not working?
+ * Fungsi tidak dapat bekerja karena fungsi tersebut mengembalikan hasil filter dalam bentuk string menggunakan metode toString(). Hal ini dapat menyebabkan hasil yang tidak diinginkan.
  */
 
 // ! Dont change this code
